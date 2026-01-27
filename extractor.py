b8075e409af2d405f15b02a6436114a7e7ba7254
Derek Kwan <dkwan6@gatech.edu>
2026-01-26 17:35:00 -0800

    add overrider

--- extractor.py
+++ extractor.py
@@ -1,4 +1,4 @@
-import sys,os,time,argparse,copy
+import sys,os,time,argparse,copy,types
 import librosa as lr
 from librosa import feature as lrf
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
@@ -7,7 +7,8 @@ import numpy as np
 import torch
 from distutils.util import strtobool
 from transformers.cache_utils import Cache, DynamicCache,EncoderDecoderCache
-from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
+from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions, BaseModelOutput, Seq2SeqLMOutput
+
 # need to override forward method of MusicgenDecoderLayer
 # https://github.com/huggingface/transformers/blob/ff13eb668aa03f151ded71636d723f2e490ad967/src/transformers/models/musicgen/modeling_musicgen.py#L304
 # change outputs so need to override forward of MusicgenDecoder
@@ -342,3 +343,392 @@ def forward_musicgenmodel(
             attentions=decoder_outputs.attentions,
             cross_attentions=decoder_outputs.cross_attentions,
         )
+
+
+#MusicforConditionalGeneration forward
+# https://github.com/huggingface/transformers/blob/main/src/transformers/models/musicgen/modeling_musicgen.py#L1606C1-L1781C1
+def mcg_forward(
+    self,
+    input_ids: torch.LongTensor | None = None,
+    attention_mask: torch.BoolTensor | None = None,
+    input_values: torch.FloatTensor | None = None,
+    padding_mask: torch.BoolTensor | None = None,
+    decoder_input_ids: torch.LongTensor | None = None,
+    decoder_attention_mask: torch.BoolTensor | None = None,
+    encoder_outputs: tuple[torch.FloatTensor] | None = None,
+    past_key_values: Cache | None = None,
+    inputs_embeds: torch.FloatTensor | None = None,
+    decoder_inputs_embeds: torch.FloatTensor | None = None,
+    labels: torch.LongTensor | None = None,
+    use_cache: bool | None = None,
+    output_attentions: bool | None = None,
+    output_hidden_states: bool | None = None,
+    return_dict: bool | None = None,
+    **kwargs,
+) -> tuple | Seq2SeqLMOutput:
+    r"""
+    padding_mask (`torch.BoolTensor` of shape `(batch_size, sequence_length)`, *optional*):
+        Mask to avoid performing attention on padding token indices. Mask values selected in `[0, 1]`:
+
+        - 1 for tokens that are **not masked**,
+        - 0 for tokens that are **masked**.
+
+        [What are attention masks?](../glossary#attention-mask)
+    decoder_input_ids (`torch.LongTensor` of shape `(batch_size * num_codebooks, target_sequence_length)`, *optional*):
+        Indices of decoder input sequence tokens in the vocabulary, corresponding to the sequence of audio codes.
+
+        Indices can be obtained by encoding an audio prompt with an audio encoder model to predict audio codes,
+        such as with the [`EncodecModel`]. See [`EncodecModel.encode`] for details.
+
+        [What are decoder input IDs?](../glossary#decoder-input-ids)
+
+        <Tip warning={true}>
+
+        The `decoder_input_ids` will automatically be converted from shape `(batch_size * num_codebooks,
+        target_sequence_length)` to `(batch_size, num_codebooks, target_sequence_length)` in the forward pass. If
+        you obtain audio codes from an audio encoding model, such as [`EncodecModel`], ensure that the number of
+        frames is equal to 1, and that you reshape the audio codes from `(frames, batch_size, num_codebooks,
+        target_sequence_length)` to `(batch_size * num_codebooks, target_sequence_length)` prior to passing them as
+        `decoder_input_ids`.
+
+        </Tip>
+    decoder_attention_mask (`torch.LongTensor` of shape `(batch_size, target_sequence_length)`, *optional*):
+        Default behavior: generate a tensor that ignores pad tokens in `decoder_input_ids`. Causal mask will also
+        be used by default.
+    labels (`torch.LongTensor` of shape `(batch_size, sequence_length, num_codebooks)`, *optional*):
+        Labels for language modeling. Note that the labels **are shifted** inside the model, i.e. you can set
+        `labels = input_ids` Indices are selected in `[-100, 0, ..., config.vocab_size]` All labels set to `-100`
+        are ignored (masked), the loss is only computed for labels in `[0, ..., config.vocab_size]`
+
+    Examples:
+    ```python
+    >>> from transformers import AutoProcessor, MusicgenForConditionalGeneration
+    >>> import torch
+
+    >>> processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+    >>> model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+
+    >>> inputs = processor(
+    ...     text=["80s pop track with bassy drums and synth", "90s rock song with loud guitars and heavy drums"],
+    ...     padding=True,
+    ...     return_tensors="pt",
+    ... )
+
+    >>> pad_token_id = model.generation_config.pad_token_id
+    >>> decoder_input_ids = (
+    ...     torch.ones((inputs.input_ids.shape[0] * model.decoder.num_codebooks, 1), dtype=torch.long)
+    ...     * pad_token_id
+    ... )
+
+    >>> logits = model(**inputs, decoder_input_ids=decoder_input_ids).logits
+    >>> logits.shape  # (bsz * num_codebooks, tgt_len, vocab_size)
+    torch.Size([8, 1, 2048])
+    ```"""
+    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+    kwargs_text_encoder = {
+        argument[len("text_encoder_")]: value
+        for argument, value in kwargs.items()
+        if argument.startswith("text_encoder_")
+    }
+
+    kwargs_audio_encoder = {
+        argument[len("audio_encoder_")]: value
+        for argument, value in kwargs.items()
+        if argument.startswith("audio_encoder_")
+    }
+
+    kwargs_decoder = {
+        argument[len("decoder_") :]: value for argument, value in kwargs.items() if argument.startswith("decoder_")
+    }
+
+    if encoder_outputs is None:
+        encoder_outputs = self.text_encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            **kwargs_text_encoder,
+        )
+    elif isinstance(encoder_outputs, tuple):
+        encoder_outputs = BaseModelOutput(*encoder_outputs)
+
+    encoder_hidden_states = encoder_outputs[0]
+
+    # optionally project encoder_hidden_states
+    if (
+        self.text_encoder.config.hidden_size != self.decoder.config.hidden_size
+        and self.decoder.config.cross_attention_hidden_size is None
+    ):
+        encoder_hidden_states = self.enc_to_dec_proj(encoder_hidden_states)
+
+    if attention_mask is not None:
+        encoder_hidden_states = encoder_hidden_states * attention_mask[..., None]
+
+    if (labels is not None) and (decoder_input_ids is None and decoder_inputs_embeds is None):
+        decoder_input_ids = shift_tokens_right(
+            labels, self.config.decoder.pad_token_id, self.config.decoder.decoder_start_token_id
+        )
+
+    elif decoder_input_ids is None and decoder_inputs_embeds is None:
+        audio_encoder_outputs = self.audio_encoder(
+            input_values=input_values,
+            padding_mask=padding_mask,
+            **kwargs_audio_encoder,
+        )
+        audio_codes = audio_encoder_outputs.audio_codes
+        frames, bsz, codebooks, seq_len = audio_codes.shape
+        if frames != 1:
+            raise ValueError(
+                f"Expected 1 frame in the audio code outputs, got {frames} frames. Ensure chunking is "
+                "disabled by setting `chunk_length=None` in the audio encoder."
+            )
+
+        if self.config.decoder.audio_channels == 2 and audio_codes.shape[2] == self.decoder.num_codebooks // 2:
+            # mono input through encodec that we convert to stereo
+            audio_codes = audio_codes.repeat_interleave(2, dim=2)
+
+        decoder_input_ids = audio_codes[0, ...].reshape(bsz * self.decoder.num_codebooks, seq_len)
+
+    # Decode
+    decoder_outputs = self.decoder(
+        input_ids=decoder_input_ids,
+        attention_mask=decoder_attention_mask,
+        encoder_hidden_states=encoder_hidden_states,
+        encoder_attention_mask=attention_mask,
+        inputs_embeds=decoder_inputs_embeds,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        use_cache=use_cache,
+        past_key_values=past_key_values,
+        return_dict=return_dict,
+        labels=labels,
+        **kwargs_decoder,
+    )
+
+    if not return_dict:
+        return decoder_outputs + encoder_outputs
+
+    return Seq2SeqLMOutput(
+        loss=decoder_outputs.loss,
+        logits=decoder_outputs.logits,
+        past_key_values=decoder_outputs.past_key_values,
+        decoder_hidden_states=decoder_outputs.hidden_states,
+        decoder_attentions=decoder_outputs.attentions,
+        cross_attentions=decoder_outputs.cross_attentions,
+        encoder_last_hidden_state=encoder_outputs.last_hidden_state,
+        encoder_hidden_states=encoder_outputs.hidden_states,
+        encoder_attentions=encoder_outputs.attentions,
+    )
+
+
+
+def override_mcg_forwards(mcg_instance):
+    mcg_model = mcg_instance.decoder.model
+    mcg_dm = mcg_model.decoder
+    mcg_layers = mcg_model.decoder.layers
+    for l in mcg_layers:
+        l.forward = types.MethodType(forward_musicgendecoderlayer, l)
+    mcg_dm.forward = types.MethodType(forward_musicgendecoder, mcg_dm)
+    mcg_model.forward = types.MethodType(forward_musicgenmodel, mcg_model)
+"""
+def get_hf_audio(f, model_sr = 44100, normalize=True):
+    audio, aud_sr = uhf.get_from_entry_syntheory_audio(f, mono=True, normalize =normalize, dur = dur)
+    if aud_sr != model_sr:
+        audio = lr.resample(audio, orig_sr=aud_sr, target_sr=model_sr)
+    return audio
+
+
+
+def path_handler(f, using_hf=False, model_sr = 44100, wav_path = None, model_type = 'jukebox', dur = 4., normalize = True, out_ext = 'dat', logfile_handle=None):
+    out_fname = None
+    audio = None
+    in_dir = um.by_projpath(wav_path)
+    in_fpath = None
+    out_fname = None
+    fname = None
+    if using_hf == False:
+        print(f'loading {f}', file=logfile_handle)
+        in_fpath = os.path.join(in_dir, f)
+        out_fname = um.ext_replace(f, new_ext=out_ext)
+        fname = um.ext_replace(f, new_ext='')
+        # don't need to load audio if jukebox
+        if model_type != 'jukebox':
+            audio = um.load_wav(f, dur = dur, normalize = normalize, sr = model_sr,  load_dir = in_dir)
+    else:
+        hf_path = f['audio']['path']
+        print(f"loading {hf_path}", file=lf)
+        out_fname = um.ext_replace(hf_path, new_ext=out_ext)
+        fname = um.ext_replace(hf_path, new_ext='')
+    aud_sr = None
+    if using_hf == True:
+        audio, aud_sr = uhf.get_from_entry_syntheory_audio(f, mono=True, normalize =normalize, dur = dur)
+        if aud_sr != model_sr:
+            audio = lr.resample(audio, orig_sr=aud_sr, target_sr=model_sr)
+    return {'in_fpath': in_fpath, 'out_fname': out_fname, 'audio': audio, 'fname': fname}
+
+
+def get_musicgen_encoder_embeddings(model, proc, audio, meanpool = True, model_sr = 32000, device='cpu'):
+    procd = proc(audio = audio, sampling_rate = model_sr, padding=True, return_tensors = 'pt')
+    procd.to(device)
+    enc = model.get_audio_encoder()
+    out = procd['input_values']
+    
+    # iterating through layers as in original syntheory codebase
+    # https://github.com/brown-palm/syntheory/blob/main/embeddings/models.py
+    for layer in enc.encoder.layers:
+        out = layer(out)
+
+    # output shape, (1, 128, 200), where 200 are the timesteps
+    # so average across timesteps for max pooling
+
+
+    if meanpool == True:
+        # gives shape (128)
+        out = torch.mean(out,axis=2).squeeze()
+    else:
+        # still need to squeeze
+        # gives shape (128, 200)
+        out = out.squeeze()
+    return out.detach().cpu().numpy()
+
+def get_musicgen_lm_hidden_states(model, proc, audio, text="", meanpool = True, model_sr = 32000, device = 'cpu'):
+    procd = proc(audio = audio, text = text, sampling_rate = model_sr, padding=True, return_tensors = 'pt')
+    procd.to(device)
+    outputs = model(**procd, output_attentions=False, output_hidden_states=True)
+    dhs = None
+    
+    #dat = None
+
+    # hidden
+    # outputs is a tuple of tensors with  shape (batch_size, seqlen, dimension) with 1 per layer
+    # torch stack makes it so we have (num_layers, batch_size, seqlen, dimension)
+    # then we average over seqlen in the meanpool case
+    # then squeeze to get rid of the 1 dim (if batch_size == 1)
+    # final shape is (num_layers, batch_size, dim)  (or (num_layers, dim) if bs=1)
+    
+    # attentions
+    # outputs is a tuple of tensors with  shape (batch_size, num_heads, seqlen, seqlen) with 1 per layer
+    # torch stack makes it so we have (num_layers, batch_size, num_heads, seqlen, sequlen)
+    # then we average over seqlens in the meanpool case
+    # then squeeze to get rid of the 1 dim (if batch_size == 1)
+    # final shape is (num_layers, batch_size, num_heads) (or (num_layers, num_heads) if bs = 1)
+
+    if meanpool == True:
+        dhs = torch.stack(outputs.decoder_hidden_states).mean(axis=2).squeeze()
+        #dat = torch.stack(outputs.decoder_attentions).mean(axis=(3,4)).squeeze()
+    else:
+        dhs = torch.stack(outputs.decoder_hidden_states).squeeze()
+        #dat = torch.stack(outputs.decoder_attentions).squeeze()
+    return dhs.detach().cpu().numpy()
+
+
+def get_embeddings(cur_act_type, cur_dataset, layers_per = 4, layer_num = -1, normalize = True, dur = 4., use_64bit = True, logfile_handle=None, recfile_handle = None, memmap = True, pickup = False, other_projdir = ''):
+    cur_model_type = um.get_model_type(cur_act_type)
+    model_sr = um.model_sr[cur_model_type]
+    model_longhand = um.model_longhand[cur_act_type]
+
+    using_hf = cur_dataset in um.hf_datasets
+    # musicgen stuff
+    device = 'cpu'
+    num_layers = None
+    proc = None
+    model = None
+    text = ""
+    wav_path = os.path.join(um.by_projpath('wav'), cur_dataset)
+    cur_pathlist = None
+    out_ext = 'dat'
+    if memmap == False:
+        out_ext = 'npy'
+    if using_hf == True:
+        cur_pathlist = uhf.load_syntheory_train_dataset(cur_dataset)
+    else:
+        cur_pathlist = um.path_list(wav_path)
+
+
+    if torch.cuda.is_available() == True:
+        device = 'cuda'
+        torch.cuda.empty_cache()
+        torch.set_default_device(device)
+
+
+    model_str = f"facebook/{cur_model_type}" 
+
+    proc = AutoProcessor.from_pretrained(model_str)
+    model = MusicgenForConditionalGeneration.from_pretrained(model_str, device_map=device)
+    model_sr = model.config.audio_encoder.sampling_rate
+
+    #print('file,is_extracted', file=rf)
+
+    # existing files removing latest (since it may be partially written) and removing extension for each of checking
+    existing_name_set = None
+    if pickup == True:
+        _file_dir = um.get_model_act_path(cur_act_type, acts_folder = acts_folder, dataset=cur_dataset, return_relative = False, make_dir = False, other_projdir = other_projdir)
+        existing_files = um.remove_latest_file(_file_dir, is_relative = False)
+        existing_name_set = set([um.get_basename(_f, with_ext = False) for _f in existing_files])
+    for fidx,f in enumerate(cur_pathlist):
+        if pickup == True:
+            cur_name = um.get_basename(f, with_ext = False)
+            if cur_name in existing_name_set:
+                continue
+        fdict = path_handler(f, model_sr = model_sr, wav_path = wav_path, normalize = normalize, dur = dur,model_type = cur_model_type, using_hf = using_hf, logfile_handle=logfile_handle, out_ext = out_ext)
+        #outpath = os.path.join(out_dir, outname)
+        out_fname = fdict['out_fname']
+        fpath = fdict['in_fpath']
+        audio = fdict['audio']
+        # store by cur_act_type (model shorthand)
+        emb_file = None
+        np_arr = None
+        if memmap == True:
+            emb_file = um.get_embedding_file(cur_act_type, acts_folder=acts_folder, dataset=cur_dataset, fname=out_fname, use_64bit = use_64bit, write=True, use_shape = None, other_projdir = other_projdir)
+        if cur_model_type == 'jukebox':
+            print(f'--- extracting jukebox for {f} with {layers_per} layers at a time ---', file=logfile_handle)
+            # note that layers are 1-indexed in jukebox
+            # so let's 0-idx and then add 1 when feeding into jukebox fn
+            layer_gen = (list(range(l, min(um.model_num_layers['jukebox'], l + layers_per))) for l in range(0,um.model_num_layers['jukebox'], layers_per))
+            has_last_layer = False
+            if memmap == False:
+                np_shape = um.get_embedding_shape(cur_act_type)
+                np_arr = np.zeros(np_shape)
+            if layer_num > 0:
+                # 0-idx from 1-idxed argt
+                layer_gen = ([l-1] for l in [layer_num])
+            for layer_arr in layer_gen:
+                # 1-idx for passing into fn
+                j_idx = [l+1 for l in layer_arr]
+                has_last_layer = um.model_num_layers['jukebox'] in j_idx
+                print(f'extracting layers {j_idx}', file=logfile_handle)
+                rep_arr = get_jukebox_layer_embeddings(fpath=fpath, audio = audio, layers=j_idx)
+                if memmap == True:
+                    emb_file[layer_arr,:] = rep_arr
+                    emb_file.flush()
+                else:
+                    np_arr[layer_arr,:] = rep_arr
+                    # should be the last layer to save
+                    if has_last_layer == True:
+                        um.save_npy(np_arr, out_fname, cur_act_type, acts_folder = acts_folder, dataset=cur_dataset, other_projdir = other_projdir)
+        else:
+            audio_ipt = fdict['audio']
+            if model_longhand == "musicgen-encoder":
+                print(f'--- extracting musicgen-encoder for {f} ---', file=logfile_handle)
+
+                rep_arr = get_musicgen_encoder_embeddings(model, proc, audio_ipt, meanpool = True, model_sr = model_sr, device=device)
+                if memmap == True:
+                    emb_file[:,:] = rep_arr
+                    emb_file.flush()
+                else:
+                    um.save_npy(rep_arr, out_fname, cur_act_type, acts_folder = acts_folder, dataset=cur_dataset, other_projdir = other_projdir)
+            else:
+
+                print(f'--- extracting musicgen_lm for {f} ---', file=logfile_handle)
+                rep_arr =  get_musicgen_lm_hidden_states(model, proc, audio_ipt, text="", meanpool = True, model_sr = model_sr, device=device)
+                if memmap == True:
+                    emb_file[:,:] = rep_arr
+                    emb_file.flush()
+                else:
+                    um.save_npy(rep_arr, out_fname, cur_act_type, acts_folder = acts_folder, dataset=cur_dataset, other_projdir = other_projdir)
+        fname = fdict['fname']
+        print(f'{fname},1', file=recfile_handle)
+"""
