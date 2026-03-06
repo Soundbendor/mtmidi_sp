a455a8bf9146673b96b95ad795d47bdb166ff963
Derek Kwan <dkwan6@gatech.edu>
2026-03-06 14:20:07 -0800

    add stg

--- models/stg.py
+++ models/stg.py
@@ -5,27 +5,27 @@ import torch
 from torch.nn import functional as F
 
 from .mlpprobe import MLPProbe
-class ConcreteAutoencoder(nn.Module):
-    def __init__(self, in_dim =4096, k = 1024, tau = 1, hard = False, hidden_dims = [], out_dim = 10, dropout = 0.5, initial_dropout = True, generator = None):
+class StochasticGates(nn.Module):
+    def __init__(self, in_dim =4096, sigma = 1, add_noise = True, hidden_dims = [], out_dim = 10, dropout = 0.5, initial_dropout = True):
         super().__init__()
         self.in_dim = in_dim
         self.k = k
-        self.tau = tau
-        self.hard = hard
+        self.sigma = sigma
+        self.add_noise = add_noise
        
         # need k gumbel_softmax layers with dimension in_dim
         # to select k elements from in_dim elements
-        self.probs = nn.Parameter(torch.empty((k, in_dim), dtype=self.ftype, requires_grad = True))
-        nn.init.uniform_(self.probs, generator = generator)
+        self.probs = nn.Parameter(torch.ones(in_dim, dtype=self.ftype, requires_grad = True) * 0.5)
         self.classifier = MLPProbe(in_dim = k, hidden_dims = hidden_dims, out_dim = out_dim, dropout = dropout, initial_dropout = initial_dropout)
         
   
-    def set_tau(self, tau):
-        self.tau = tau
+    def set_sigma(self, sigma):
+        self.sigma = sigma
 
-    def set_hard(self, hard):
-        self.hard = hard
+    def set_add_noise(self,add_noise):
+        self.add_noise = add_noise
 
+    # to do
     def forward(self, x):
         feature_weights = F.gumbel_softmax(self.probs, tau = self.tau, hard = self.hard)
         weighted_input = x @ feature_weights.T # (batch_size, in_dim) @ (in_dim, k)
